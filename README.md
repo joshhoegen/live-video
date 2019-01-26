11c6403a0b79b2b2b48e1dc8aab83bbb455894e8
joshuahoegen <josh.hoegen@natgeo.com>
2019-01-25 20:34:18 -0500

    Adding camera selector

--- README.md
+++ README.md
@@ -3,6 +3,14 @@
 Tiny! No Deps! 1,127 bytes!
 
 ```js
+// JUST CALL VideoToCanvas(video, audio, camera)
+/**
+ * Return chosen camera. Return default true value if only one exists
+ * @param {Object} video - Required. A video element.
+ * @param {bool} audio - Optional. Use audio?
+ * @param {Number} camera - Optional. Which camera to use.
+ */
+
 // Simply add a video node
 import LiveVideo from 'live-video';
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,44 @@
 const LiveVideo = class {
-  constructor(video, audio = true) {
+  /**
+   * Return chosen camera. Return default true value if only one exists
+   * @param {Object} video - A video element.
+   * @param {bool} audio - Use audio?
+   * @param {Number} camera - Which camera to use.
+   */
+  constructor(video, audio = true, camera = 0) {
     this.video = video;
     this.audio = audio;
+    this.activeCamera = camera;
+    this.vidVal = this.getCamera();
+  }
+
+  getCamera() {
+    const cameras = [];
+    const getDevices = navigator.mediaDevices.enumerateDevices();
+
+    return getDevices.then((info) => {
+      for (let i = 0; i < info.length; i += 1) {
+        if (info[i].kind === 'videoinput') {
+          cameras.push(info[i]);
+        }
+      }
+
+      return {
+        deviceId: {
+          exact: cameras[this.activeCamera].deviceId
+        }
+      };
+    }).catch(() => true);
   }
 
   play() {
-    const { audio, video } = this;
+    const {
+      audio,
+      video
+    } = this;
     if (video && navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       return navigator.mediaDevices.getUserMedia({
-        video: true,
+        video: this.vidVal,
         audio
       }).then((stream) => {
         try {
